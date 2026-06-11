6401449f6c0ac3e3c618ad14228bb4df7d1315bf
Halley <16331094+Halley123@user.noreply.gitee.com>
2026-06-11 20:04:14 +0800

    ui: 登录页面验证/注册按钮尺寸缩小至70%

--- gui_login.py
+++ gui_login.py
@@ -1,4 +1,4 @@
-"""Login window with voiceprint verification and enrollment."""
+﻿"""Login window with voiceprint verification and enrollment."""
 import os
 import time
 import numpy as np
@@ -290,16 +290,16 @@ class LoginWindow(QWidget):
         )
 
         self.verify_btn = QPushButton("  \u5f00\u59cb\u58f0\u7eb9\u9a8c\u8bc1")
-        self.verify_btn.setFixedHeight(180)
-        self.verify_btn.setMinimumWidth(700)
+        self.verify_btn.setFixedHeight(126)
+        self.verify_btn.setMinimumWidth(490)
         self.verify_btn.setCursor(Qt.PointingHandCursor)
         self.verify_btn.setStyleSheet(btn_style_grad)
         self.verify_btn.clicked.connect(self._start_verify)
         btn_row.addWidget(self.verify_btn)
 
         self.enroll_btn = QPushButton("  \u6ce8\u518c\u65b0\u7528\u6237")
-        self.enroll_btn.setFixedHeight(180)
-        self.enroll_btn.setMinimumWidth(700)
+        self.enroll_btn.setFixedHeight(126)
+        self.enroll_btn.setMinimumWidth(490)
         self.enroll_btn.setCursor(Qt.PointingHandCursor)
         self.enroll_btn.setStyleSheet(
             "QPushButton {"
